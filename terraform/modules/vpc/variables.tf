973069e823ea31e9d38e13258942242efc2c7437
naapholei <annettetettey@gmail.com>
2026-07-03 22:25:35 +0000

    minor vpc name

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,13 +1,14 @@
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
+  default     = "zuriapp-vpc"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
-}
+} 
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
