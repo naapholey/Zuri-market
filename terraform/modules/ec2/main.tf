00ccae88fc514f816daf3498c683b575ed15bb29
naapholei <annettetettey@gmail.com>
2026-07-04 20:27:02 +0000

    add import

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -14,11 +14,11 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-/* import {
+ import {
   to = aws_iam_role.iam_role
   id = zuriapp-iam-role//"arn:aws:iam::870737143368:role/zuriapp-iam-role"
 }
- */
+ 
 resource "aws_iam_role" "iam_role" {
   name = var.iam_role
 
@@ -35,10 +35,10 @@ resource "aws_iam_role" "iam_role" {
     ]
   })
 }
-/* import {
+ import {
   to = aws_iam_policy.iam_policy
   id = "arn:aws:iam::870737143368:policy/zuriapp-secrets-policy"
-} */
+} 
 resource "aws_iam_policy" "iam_policy" {
   name = var.iam_policy
 
@@ -67,10 +67,10 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
 
 #------------ INSTANCE PROFILE ------------ 
 
-/* import {
+ import {
   to = aws_iam_instance_profile.instance_profile
   id = "zuriapp-ec2-profile"
-} */
+} 
 resource "aws_iam_instance_profile" "instance_profile" {
   name = var.instance_profile
   role = aws_iam_role.iam_role.name
