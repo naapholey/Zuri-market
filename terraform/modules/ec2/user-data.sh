8666d3875e96ffe40e0091bb4c6fb2595c1ad300
naapholei <annettetettey@gmail.com>
2026-07-05 21:38:31 +0000

    new ec2 specs

--- terraform/modules/ec2/user-data.sh
+++ terraform/modules/ec2/user-data.sh
@@ -6,15 +6,9 @@ apt-get install -y curl git ca-certificates
 
 curl -sfL https://get.k3s.io | sh -
 
-# Wait for k3s API to be ready (IMPORTANT)
-until kubectl get nodes >/dev/null 2>&1; do
-  echo "Waiting for k3s..."
-  sleep 5
-done
-
 mkdir -p /home/ubuntu/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config
 chown -R ubuntu:ubuntu /home/ubuntu/.kube
 chmod 600 /home/ubuntu/.kube/config
 
-echo "export KUBECONFIG=/home/ubuntu/.kube/config" >> /home/ubuntu/.bashrc
+echo 'export KUBECONFIG=/home/ubuntu/.kube/config' >> /home/ubuntu/.bashrc
